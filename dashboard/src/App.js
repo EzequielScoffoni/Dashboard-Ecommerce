8fbf8fb8339bd22c6b24126bf4dad154f6891882
Ezequiel Scoffoni <eze.esco@gmail.com>
2025-10-18 14:10:39 -0300

    carga de dashboard

---
+++ dashboard/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+import GroupExample from './components/Detail';
+import BasicExample from './components/User';
+import TabsExample from './components/Navbar';
+import Productos from './components/Productos';
+
+import { Link, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div >
+      <div>
+      <br/>  
+      <br/>  
+      <h2 className='titulo'>Dashboards DDecoHouse</h2>
+      </div>
+        <br/>
+        <div>
+        <GroupExample />
+        <BasicExample />
+        </div>
+        <br/>
+    </div>
+  );
+}
+
+export default App;
